abaa6e710934c63593e8456ce8ffc7bcdd217389
Rajan Gupta <31067215+Rajan-corient@users.noreply.github.com>
2023-03-06 13:01:36 +0530

    changes added

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -14,8 +14,19 @@ export const Home = ({ carList, title }) => {
           <li tabIndex={0}>Audi</li>
           {/* { carList.map((car, index) => <Car key={car.id} brand={car.brand} index={index}></Car>) } */}
         </ul> : 'No Records Found'
-        
       }
+
+        <div class="card m-4">
+          <img src="image.png" alt="proper alt text" />
+          <h3>Heading</h3>
+          <p class="lede">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis tenetur iste vero fuga error dolor quaerat optio adipisci inventore, aspernatur a voluptatum culpa unde ratione doloremque voluptatibus illo recusandae hic.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt corporis veritatis impedit neque hic iste, debitis libero deleniti minima saepe praesentium fuga in facere aliquam ratione commodi laudantium cum aut!</p>
+        </div>
+
+      <button className='mx-4 my-2 btn btn-info'>Real Buttton</button>
+      <div tabIndex={0} role={'button'} style={{width: '10%'}} className='mx-4 my-2 p-2 bg-warning text-center rounded'>
+        Simulated Buttton
+      </div>
     </>
   )
 }
